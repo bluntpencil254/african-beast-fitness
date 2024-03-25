5af5d59b995a060637dcc19ad6cdfb18685cf492
bluntpencil254 <makorimike9@gmail.com>
2024-03-25 10:41:20 +0300

    styling for navbar

--- african-beast-fitness/src/App.js
+++ african-beast-fitness/src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
      <p>coming soon</p>
      
     </div>
---
+++ african-beast-fitness/src/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return ( 
+       <div className="navbar">
+         <div className="name">
+            <h1>AFRICAN BEAST FITNESS</h1>
+        </div>
+        <div className="links">
+            <a href="/">Plans</a>
+            <a href="/">Catalogue</a>
+            <a href="/">Open Days</a>
+            <a href="/">Locations</a>
+            <a href="/">Contact Us</a>
+            
+        </div>
+       </div>
+     );
+}
+ 
+export default Navbar;
